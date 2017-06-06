f7e02151c6b7f8441f73c0be6a34bb225fc446db
mathan898 <mathansri898@gmail.com>
2017-06-06 14:36:21 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class repeat
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String n=new String();
+n=s.nextLine();
+for(int i=0;i<5;i++)
+{
+System.out.println(n);
+}
+}
+}
