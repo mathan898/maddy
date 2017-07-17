7e83a8b01a9a86fc1b28a692fa9c07e5b8cad7b2
mathan898 <mathansri898@gmail.com>
2017-07-17 12:10:23 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class commaa
+{
+public static void main(String args[])
+{
+for(int i=0;i<args.length;i++)
+{
+if(Integer.parseInt(args[i])==i)
+{
+System.out.print(args[i]+" ");
+}
+}
+}
+}
