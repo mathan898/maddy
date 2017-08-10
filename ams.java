da2bc0ffb0436544d1d5b0f332984a4e9555da9a
mathan898 <mathansri898@gmail.com>
2017-08-10 17:19:22 +0530

    Create ams.java

---
+++ ams.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class ams
+{
+public static void main(String str[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a, b=0, c=n;
+while(n>0)
+{
+a=n%10;  
+n=n/10;  
+b=b+(a*a*a);
+}
+if(c==b) 
+System.out.println("Armstrong number");
+else      
+System.out.println(" Not Armstrong number");
+}
+}
