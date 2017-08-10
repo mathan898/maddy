a75f17ae1b3f075e3b4faab6e1a62af031fb73d8
mathan898 <mathansri898@gmail.com>
2017-08-10 16:32:16 +0530

    Create new file

---
+++ 0ddint.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class oddint
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+int n=ss.nextInt(),flag=0;
+int a[]=new int[n];
+int i;
+for(i=0;i<n;i++)
+{
+a[i]=ss.nextInt();
+}
+for(i=0;i<n;i++)
+{
+if(a[i]%2==0)
+{
+System.out.print(a[i]+" ");
+}
+}
+}
+}
