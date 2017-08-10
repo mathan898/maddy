afbd8429eb21b5d72848048a8e8fa66f149c5638
mathan898 <mathansri898@gmail.com>
2017-08-10 21:35:22 +0530

    Create conver.java

---
+++ conver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class conver
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+String s=ss.nextLine();
+int s1=Integer.parseInt(s);
+System.out.print(s1);
+}
+}
